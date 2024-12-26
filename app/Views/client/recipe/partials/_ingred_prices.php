32502b6ab0333803e079e7b269d92d07dbe4a40c
shetaraven <68856022+shetaraven@users.noreply.github.com>
2024-12-26 13:17:04 +0800

    changes

--- app/Views/client/recipe/partials/_ingred_prices.php
+++ app/Views/client/recipe/partials/_ingred_prices.php
@@ -1,19 +1,17 @@
 <thead>
     <tr>
-        <th scope="col">Ingredient</th>
-        <th scope="col">Quantity</th>
+        <th scope="col" style="border:none;"></th>
         <?php foreach ($stores_used as $key => $store_data) : ?>
-            <th scope="col"><?=$store_data['STORE_NAME']?> Price</th>
+            <th scope="col" class="store-name"><span style="font-size: 2em;color:  var(--link-hover-color);"><?=$store_data['STORE_NAME']?></span><br/>Price</th>
         <?php endforeach; ?>
     </tr>
 </thead>
 <tbody>
     <?php foreach ($recipe_ingredients as $key => $ri_info) : ?>
         <tr>
-            <td data-label="Ingredient"><?= $ri_info['NAME'] ?></td>
-            <td data-label="Quantity"><?= $ri_info['VOLUME'] ?><?= $ri_info['UNIT_MEASURE_LABEL'] ?></td>
+            <td data-label="Ingredient"><?= $ri_info['VOLUME'] ?><?= $ri_info['UNIT_MEASURE_LABEL'] ?>&nbsp;<?= $ri_info['NAME'] ?></td>
             <?php foreach ($stores_used as $key => $store_data) : ?>
-                <th scope="col"><?=isset( $ri_info['STORE_PRICES'][$store_data['STORE_ID']]) ? $ri_info['STORE_PRICES'][$store_data['STORE_ID']] : '--'?></th>
+                <th scope="col"><?=isset( $ri_info['STORE_PRICES'][$store_data['STORE_ID']]) ? $ri_info['STORE_PRICES'][$store_data['STORE_ID']] . ' <span class="currency">SEK</span>' : '--'?></th>
             <?php endforeach; ?>
         </tr>
     <?php endforeach; ?>
