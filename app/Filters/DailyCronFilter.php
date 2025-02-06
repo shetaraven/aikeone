c1eb8d409979a6b7c4532c7b4c0ec5c74b440664
Mico <jaymico.bautista@gmail.com>
2025-02-06 08:17:12 +0800

    Add condition for daily featured recipe

--- app/Filters/DailyCronFilter.php
+++ app/Filters/DailyCronFilter.php
@@ -20,9 +20,11 @@ class DailyCronFilter implements FilterInterface
         if (date('Y-m-d', strtotime($roth_info['UPDATED_AT'])) !== date('Y-m-d')) {
             $random_recipe = new RecipesModel();
             $random_recipe = $random_recipe->orderBy('RAND()')->first();
+            if( $random_recipe ) {
+                $update_featured = new SysVariablesModel();
+                $update_featured->where('LABEL', 'ROTD')->set(['VALUE' => $random_recipe['ID']])->update();
+            }
 
-            $update_featured = new SysVariablesModel();
-            $update_featured->where('LABEL', 'ROTD')->set(['VALUE' => $random_recipe['ID']])->update();
         }
     }
 
